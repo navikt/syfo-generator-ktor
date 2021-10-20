1a658d66523582e791a512f9d0ce429a320cb291
Tia Firing <tia.firing@nav.no>
2021-10-20 12:18:26 +0200

    oppgraderer diverse versjoner

--- generators/app/templates/src/main/kotlin/no/nav/application/ApplicationServer.kt
+++ generators/app/templates/src/main/kotlin/no/nav/application/ApplicationServer.kt
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit
 class ApplicationServer(private val applicationServer: ApplicationEngine, private val applicationState: ApplicationState) {
     init {
         Runtime.getRuntime().addShutdownHook(Thread {
+            this.applicationState.ready = false
             this.applicationServer.stop(TimeUnit.SECONDS.toMillis(10), TimeUnit.SECONDS.toMillis(10))
         })
     }
